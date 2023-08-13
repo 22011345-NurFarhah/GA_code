f33efe5a784328759d8afa28e8eff1b26bc5bda0
22011345-NurFarhah <22011345@myrp.edu.sg>
2023-08-13 19:38:34 +0800

    2nd updated GA code

--- payment.js
+++ payment.js
@@ -9,29 +9,49 @@ I declare that this code was written by me.
  Date/Time Last modified: 5 August 2023 12:54 pm */
 //Pay for the transaction
 
+
 function processPayment() {
-var paymentMethod = document.querySelector('input[name="pay"]:checked');
+    var paymentMethod = document.querySelector('input[name="pay"]:checked');
 
-if (!paymentMethod) {
-alert("Please select a payment method.");
-return;
+    if (!paymentMethod) {
+        alert("Please select a payment method.");
+        return;
+    }
+
+    paymentMethod = paymentMethod.value;
+
+    if (paymentMethod === "card") {
+        var cardNumber = document.getElementById("cardNumber").value;
+        var cardholderName = document.getElementById("name").value;
+        var expiryDate = document.getElementById("expiryDate").value;
+        var cvc = document.getElementById("cvc").value;
+
+        if (cardNumber != "" && cardholderName != "" && expiryDate != "" && cvc != "") {
+            alert("Payment Successful");
+            location.href = "ConfirmPayment.html";
+        } else {
+            alert("Please fill in ALL of your card information.");
+        }
+    } else if (paymentMethod === "paypal") {
+
+    }
 }
-
-paymentMethod = paymentMethod.value;
-
-if (paymentMethod === "card") {
-var cardNumber = document.getElementById("cardNumber").value;
-var cardholderName = document.getElementById("name").value;
-var expiryDate = document.getElementById("expiryDate").value;
-var cvc = document.getElementById("cvc").value;
-
-if (cardNumber !== "" && cardholderName !== "" && expiryDate !== "" && cvc !== "") {
-alert("Payment Successful");
-location.href = "ConfirmPayment.html";
-} else {
-alert("Please fill in ALL of your card information.");
-}
-} else if (paymentMethod === "paypal") {
-location.href = "paypal.html";
-}
+function checkCard() {
+    if (document.getElementById('Creditcard').checked) {
+        document.getElementById('card').style.visibility = 'visible';
+        document.getElementById('paypal-button-container').innerHTML = '';
+    }
+    else if (document.getElementById('paymentPayPal').checked) {
+        document.getElementById('card').style.visibility = 'hidden';
+        document.getElementById('paypal-button-container').innerHTML = '';
+        paypal.Buttons({
+            style: {
+                layout: 'horizontal',
+                color: 'black',
+                shape: 'rect',
+                label: 'pay',
+            }
+        }).render('#paypal-button-container');
+        
+    }
 }
